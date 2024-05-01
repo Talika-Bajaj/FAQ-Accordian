1412dce9b5ad01b247c7a5ea0ff91ca4016dc6eb
Talika Bajaj <bajajtalika2001@gmail.com>
2024-05-01 16:06:20 +0530

    Format document

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const plus = document.getElementsByClassName('plus');
-const answers = document.querySelectorAll('.answer');
 const questions = document.getElementsByClassName('question');
 // console.log(answers);
 // console.log(questions);
 // console.log(plus);
 
+
 for (let index = 0; index < questions.length; index++) {
 
     const element = questions[index];
-    element.addEventListener('click', () => {
-        console.log('i am working');
-        if (answers[index].style.display === 'block') {
-            answers[index].style.display = 'none';
-            plus[index].src = 'assets/images/icon-plus.svg';
-        } else {
-            answers[index].style.display = 'block';
+    element.addEventListener('click', (e) => {
+        // console.log(e.target);
+        // console.log(e.target.parentElement);
+        const parent = e.target.parentElement;
+        // console.log(e.target.parentElement.open);
+        if (parent.open === false) {
+            console.log('jcbvjbvjkd');
             plus[index].src = 'assets/images/icon-minus.svg';
+        } else {
+            plus[index].src = 'assets/images/icon-plus.svg';
+
         }
 
-        // console.log(element);      
     })
 
 }
-
